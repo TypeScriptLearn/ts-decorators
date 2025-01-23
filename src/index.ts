d49c697ca7f0870ae49b3faada9d3ebe476f8af5
Valeriy Semenenko <gearmobile@gmail.com>
2025-01-24 00:57:35 +0300

    composition decorators

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,9 @@ interface ICar {
   isOpen: () => string;
 }
 
-@modifyCar
+/** changeFuelStatus(changeDoorStatus(obj)) */
+@changeFuelStatus(110)
+@changeDoorStatus(false)
 class Car implements ICar {
   isOpen() {
     console.log(`Current fuel level is ${this.fuel}`);
@@ -19,18 +21,22 @@ class Car implements ICar {
   ) {}
 }
 
-const car: ICar = new Car('100%', true, 3);
-
-function modifyCar<T extends { new (...args: any[]): {} }>(constructor: T): T {
-  return class extends constructor {
-    open = false;
+function changeDoorStatus(status: boolean) {
+  return <T extends { new (...args: any[]): {} }>(constructor: T): T => {
+    return class extends constructor {
+      open = status;
+    };
   };
 }
 
-function modifyFuel(car: ICar): ICar {
-  car.fuel = '110%';
-
-  return car;
+function changeFuelStatus(amount: number) {
+  return <T extends { new (...args: any[]): {} }>(constructor: T): T => {
+    return class extends constructor {
+      fuel = `${amount}%`;
+    };
+  };
 }
+
+const car: ICar = new Car('100%', true, 3);
 
 console.log(car.isOpen());
