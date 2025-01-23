fec553b8449c17b8ac8dc0836300bfc1e11b4d2e
Valeriy Semenenko <gearmobile@gmail.com>
2025-01-23 22:45:50 +0300

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ["plugin:@typescript-eslint/recommended", "eslint:recommended"],
+  overrides: [],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {},
+  root: true,
+};
